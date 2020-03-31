e05055ced52bdbc203019c5f45793d8c08b44f58
vk59 <jkostylev592@gmail.com>
2020-03-31 22:17:19 +0300

    Add LoadingActivity.

---
+++ app/src/main/java/club/infolab/testchart/LoadingActivity.java
@@ -0,0 +1,26 @@
+package club.infolab.testchart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
