6018674daf5fcd3336c06b243d2ea3c59fb0fd23
vk59 <jkostylev592@gmail.com>
2020-03-31 22:40:24 +0300

    Add Picasso library.

--- app/src/main/java/club/infolab/testchart/LoadingActivity.java
+++ app/src/main/java/club/infolab/testchart/LoadingActivity.java
@@ -3,8 +3,11 @@ package club.infolab.testchart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
 
 public class LoadingActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
@@ -13,6 +16,8 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        ImageView imageView = findViewById(R.id.loading_logo);
+        Picasso.get().load(R.drawable.isc_logo).into(imageView);
 
         new Handler().postDelayed(new Runnable() {
             @Override
