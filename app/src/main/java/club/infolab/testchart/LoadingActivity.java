b7052d96b4b3fa869603a4b3a5af91607d6b88ef
JustWorking42 <german.ust2000@gmail.com>
2020-04-01 17:50:02 +0500

    Remade the Bluetooth check.

--- app/src/main/java/club/infolab/testchart/LoadingActivity.java
+++ app/src/main/java/club/infolab/testchart/LoadingActivity.java
@@ -1,5 +1,6 @@
 package club.infolab.testchart;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,6 +12,9 @@ import com.squareup.picasso.Picasso;
 
 public class LoadingActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
+    private static int REQUEST_ENABLE_BLUETOOTH = 0;
+    private boolean wasLogoShow;
+    private boolean wasBluetoothEnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,42 @@ public class LoadingActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.loading_logo);
         Picasso.get().load(R.drawable.isc_logo).into(imageView);
 
+        checkBluetoothEnable();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
+                wasLogoShow = true;
+                startMainActivity();
             }
         }, SPLASH_TIME_OUT);
     }
+
+    private void checkBluetoothEnable() {
+        BluetoothAdapter bluetoothAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter.isEnabled()) {
+            wasBluetoothEnable = true;
+        }
+        else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+            }
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        checkBluetoothEnable();
+    }
+
+    private void startMainActivity() {
+        if (wasLogoShow && wasBluetoothEnable) {
+            Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
 }
+
+
+
--- app/src/main/java/club/infolab/testchart/MainActivity.java
+++ app/src/main/java/club/infolab/testchart/MainActivity.java
@@ -1,26 +1,13 @@
 package club.infolab.testchart;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private static int REQUEST_ENABLE_BT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        CheckEnable();
-    }
-
-    public void CheckEnable(){
-        BluetoothAdapter bluetoothAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
-        if (! bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent ( BluetoothAdapter . ACTION_REQUEST_ENABLE );
-            startActivityForResult ( enableBtIntent, REQUEST_ENABLE_BT );
-        }
     }
 }
