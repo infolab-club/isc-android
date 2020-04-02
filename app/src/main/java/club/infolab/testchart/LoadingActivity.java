73d0867278cbd3d91aa74bfd4151abe3b1ff3bf3
JustWorking42 <german.ust2000@gmail.com>
2020-04-02 20:13:10 +0500

    Added reading lines from files.

--- app/src/main/java/club/infolab/testchart/LoadingActivity.java
+++ app/src/main/java/club/infolab/testchart/LoadingActivity.java
@@ -10,11 +10,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class LoadingActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
     private static int REQUEST_ENABLE_BLUETOOTH = 0;
     private boolean wasLogoShow;
     private boolean wasBluetoothEnable;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,9 @@ public class LoadingActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.loading_logo);
         Picasso.get().load(R.drawable.isc_logo).into(imageView);
 
-        //Разбивка данных на объекты
-
-//        GetData getData = new GetData();
-//        getData.Make();
-
+        //Считывание файла и последующая разбивка данных на объекты
+        Reader();
+        //Проверка Bluetooth
         checkBluetoothEnable();
 
         new Handler().postDelayed(new Runnable() {
@@ -61,6 +65,21 @@ public class LoadingActivity extends AppCompatActivity {
             Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
+        }
+    }
+
+    private void Reader() {
+        CurrentTest currentTest = new CurrentTest();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("sample_input/cyclic.txt")));
+            String line = reader.readLine();
+            currentTest.AppendMomentTest(line);
+            while (line != null){
+                currentTest.AppendMomentTest(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
