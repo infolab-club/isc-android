facc7d3d3d515477c4cc003008450147fe27915d
ivan <jkostylev592@gmail.com>
2020-05-26 23:09:03 +0300

    Added icon; added dividers between Tabs.

--- app/src/main/java/club/infolab/isc/MainActivity.java
+++ app/src/main/java/club/infolab/isc/MainActivity.java
@@ -1,7 +1,10 @@
 package club.infolab.isc;
 
 import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -41,6 +44,20 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        setDividerOnTab(tabLayout);
+    }
+
+    private void setDividerOnTab(TabLayout tabLayout) {
+        View root = tabLayout.getChildAt(0);
+        if (root instanceof LinearLayout) {
+            ((LinearLayout) root).setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+            GradientDrawable drawable = new GradientDrawable();
+            drawable.setColor(getResources().getColor(R.color.separatorTab));
+            drawable.setSize(2, 1);
+            ((LinearLayout) root).setDividerPadding(10);
+            ((LinearLayout) root).setDividerDrawable(drawable);
+        }
     }
 
     @Override
